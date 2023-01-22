b3e900a65c9e065e7c620c2816e9402674513fff
Mariia Safronova <mary.an.safronova@yandex.ru>
2023-01-22 16:02:07 +0300

    fix: массив с начальными карточками перенесен в файл constants

--- src/components/card.js
+++ src/components/card.js
@@ -1,5 +1,6 @@
 // Функции для работы с карточками проекта
 import { openPopup, closePopup } from "./utils";
+import { initialCards } from "./constants";
 
 export { handleAddFormSubmit };
 export { cardAddFormEl, cardAddPopupEl };
@@ -13,27 +14,6 @@ const imgPopupEl = cardImgPopupEl.querySelector('.popup__image');
 const imgPopupCaptionEl = cardImgPopupEl.querySelector('.popup__img-caption');
 const cardAddPopupEl = document.querySelector('.popup-add');
 const cardAddFormEl = document.querySelector('.add-form');
-
-// Новый массив карточек
-const initialCards = [{
-  name: 'Архыз',
-  link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-}, {
-  name: 'Челябинская область',
-  link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-}, {
-  name: 'Иваново',
-  link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-}, {
-  name: 'Камчатка',
-  link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-}, {
-  name: 'Холмогорский район',
-  link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-}, {
-  name: 'Байкал',
-  link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-}];
 
 // Добавление карточек из массива
 const cardsArr = initialCards.map(function (item) {
