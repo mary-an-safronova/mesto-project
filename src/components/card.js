75d053c06e791c83f61dad042d611399eeb69a61
Mariia Safronova <mary.an.safronova@yandex.ru>
2023-01-29 02:17:04 +0300

    feat: реализовано удаление карточки с сервера

--- src/components/card.js
+++ src/components/card.js
@@ -1,19 +1,62 @@
 // Функции для работы с карточками проекта
-import { openPopup } from "./utils";
+import { openPopup, closePopup } from "./utils";
+import { myUserId } from "..";
+import { deleteCards } from "./api";
 
 export { createCard };
-export { cardAddFormEl };
+export { cardAddFormEl, deletePopupEl, deleteFormSubmitBtnEl };
 
 const cardImgPopupEl = document.querySelector('.popup-img');
 const imgPopupEl = cardImgPopupEl.querySelector('.popup__image');
 const imgPopupCaptionEl = cardImgPopupEl.querySelector('.popup__img-caption');
-
 const cardAddFormEl = document.querySelector('.add-form');
+const deletePopupEl = document.querySelector('.popup-delete');
+const deleteFormSubmitBtnEl = document.querySelector('.delete-form__submit-button');
 
-const ownerId = 'e8113158c46c1d5bd9eb7fda';
+// Функция удаления ближайшей к корзине карточки
+const removeClosestCard = (button) => {
+  let card = button.closest('.place');
+
+  deleteCards(card.id)
+  .then((result) => {
+    if (result.ok) {
+      card.remove();
+      closePopup(deletePopupEl);
+    }
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+  deleteFormSubmitBtnEl.removeEventListener('click', () => {
+    removeClosestCard(button)
+  });
+}
+
+// Функция открытия попапа подтверждения удаления карточки
+const openDeletePopup = (button) => {
+  openPopup(deletePopupEl);
+  deleteFormSubmitBtnEl.classList.remove('form__submit-button_inactive');
+  deleteFormSubmitBtnEl.addEventListener('click', () => {
+    removeClosestCard(button)
+  });
+  button.removeEventListener('click', openDeletePopup);
+}
+
+// Функция проверки id пользователя для определения принадлежности карточки
+const checkOwnerId = (button, id) => {
+  if (id !== myUserId) {
+    button.classList.add('place__delete-button_hidden');
+  } else {
+    button.classList.remove('place__delete-button_hidden');
+    button.addEventListener('click', () => {
+      openDeletePopup(button);
+    });
+  }
+}
 
 // Функция создания новых карточек
-function createCard(template, name, link, likes, id) {
+function createCard(template, name, link, likes, id, cardId) {
   const cardElement = template.querySelector('.place').cloneNode(true);
   const cardImg = cardElement.querySelector('.place__image');
   const likeElement = cardElement.querySelector('.place__like');
@@ -24,25 +67,34 @@ function createCard(template, name, link, likes, id) {
   cardImg.classList.add('place__image_size');
   cardImg.alt = name;
   likeCounterEl.textContent = likes.length;
+  cardElement.id = cardId;
+
   // Лайк
   likeElement.addEventListener('click', () => {
     likeElement.classList.toggle('place__like_active');
   });
+
   // Корзина
-  if (ownerId !== id) {
-    deleteBtnElement.classList.add("place__delete-button_hidden");
-  } else {
-    deleteBtnElement.addEventListener('click', () => {
-      deleteBtnElement.closest('.place').remove();
-    });
-  }
+  checkOwnerId(deleteBtnElement, id, cardId);
+
   // Открытие попапов изображений карточек (в т.ч. новых)
-  cardImg.addEventListener('click', () => {
+  const openCardImage = () => {
     openPopup(cardImgPopupEl);
     imgPopupEl.src = link;
     imgPopupCaptionEl.textContent = name;
     imgPopupEl.alt = name;
-  });
+    cardImg.removeEventListener('click', openCardImage);
+  }
+
+  cardImg.addEventListener('click', openCardImage);
 
   return cardElement
 }
+
+// Обработчик удаления карточки
+function handleDeleteFormSubmit(evt) {
+  evt.preventDefault();
+}
+
+// Слушатель submit удаления карточек
+deletePopupEl.addEventListener('submit', handleDeleteFormSubmit);
