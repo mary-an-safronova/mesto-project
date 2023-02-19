e496e6b280b7a5d39d6635697eccded1c86b5eb3
Mariia Safronova <mary.an.safronova@yandex.ru>
2023-02-20 02:27:26 +0300

    fix: исправлено задвоение события submit

--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -24,6 +24,7 @@ export default class PopupWithForm extends Popup {
     super.setEventListeners();
     this.form.addEventListener('submit', (evt) => {
       evt.preventDefault();
+      evt.stopImmediatePropagation();
 
       this.handleFormSubmit(this._getInputValues())
     });
